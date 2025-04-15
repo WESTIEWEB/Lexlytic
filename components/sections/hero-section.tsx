29b7e80dd6c08dd7a666acc459c24da8ff735fec
harllogeen <harllogeen2020@gmail.com>
2025-04-15 21:47:21 +0100

    new

--- components/sections/hero-section.tsx
+++ components/sections/hero-section.tsx
@@ -13,7 +13,7 @@ export function HeroSection() {
           className="w-full h-full object-cover object-center"
         >
           <source src="/video/africa.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
+          Your browser does not support the video tag
         </video>
         <div className="absolute inset-0 bg-blue-900 bg-opacity-70" />
       </div>
