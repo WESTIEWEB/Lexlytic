0a65b5c4101b76820cb12fe012242a563af80de8
CHIBUIKE ILO <82292997+WESTIEWEB@users.noreply.github.com>
2025-04-25 03:08:20 +0100

    minor update

--- components/sections/hero-section.tsx
+++ components/sections/hero-section.tsx
@@ -60,7 +60,7 @@ export function HeroSection() {
       playsInline
       className="w-full h-full object-cover object-center"
     >
-      <source src="/globalmap.mp4" type="video/mp4" />
+      <source src="/videos/globalmap.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
     
